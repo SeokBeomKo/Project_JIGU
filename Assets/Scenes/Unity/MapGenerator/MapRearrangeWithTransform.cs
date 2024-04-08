3cc528d8c40a0c13ac0b82c5623e3a21ff83bef4
mangruf <pinky0927@naver.com>
2024-04-08 22:14:21 +0900

    Update : 무한맵 생성 코드 수정 타일의 실제 위치 변경되도록 설정

--- Assets/Scenes/Unity/MapGenerator/MapRearrangeWithTransform.cs
+++ Assets/Scenes/Unity/MapGenerator/MapRearrangeWithTransform.cs
@@ -2,48 +2,50 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+public enum Direction
+{
+    UP,
+    DOWN,
+    LEFT,
+    RIGHT
+}
+
+
 public class MapRearrangeWithTransform : MonoBehaviour
 {
     public Transform[] tiles = new Transform[9];
-    private Vector3[,] positions = new Vector3[3, 3];
+    private Vector3[,] positions = new Vector3[3, 3]; 
+    private int mapSize = 30; // 맵 하나의 크기 30 X 30
 
     private void Start()
     {
         int index = 0;
-        for (int i = 0; i < 3; i++) // y 축에 대응
+        for (int i = 0; i < 3; i++) // y 축
         {
-            for (int j = 0; j < 3; j++) // x 축에 대응
+            for (int j = 0; j < 3; j++) // x 축
             {
-                // positions 배열에 직접적인 Unity 좌표계 매핑 없이 위치 설정
-                positions[i, j] = new Vector3(j - 1, -(i - 1), 0); // x, y 위치 조정
+                // positions 배열에 Unity 좌표계 매핑 없이 직접적인 위치 설정
+                positions[i, j] = new Vector3((j - 1) * mapSize, -(i - 1) * mapSize, 0); // x, y 위치를 30 단위로 조정
                 tiles[index].position = positions[i, j];
                 index++;
             }
         }
     }
 
-    public enum Direction
-    {
-        Up,
-        Down,
-        Left,
-        Right
-    }
-
     public void Move(Direction direction)
     {
         switch (direction)
         {
-            case Direction.Right:
+            case Direction.RIGHT:
                 MoveHorizontal(1);
                 break;
-            case Direction.Left:
+            case Direction.LEFT:
                 MoveHorizontal(-1);
                 break;
-            case Direction.Up:
+            case Direction.UP:
                 MoveVertical(-1);
                 break;
-            case Direction.Down:
+            case Direction.DOWN:
                 MoveVertical(1);
                 break;
         }
@@ -51,29 +53,29 @@ public class MapRearrangeWithTransform : MonoBehaviour
         UpdateTilePositions();
     }
 
-    private void MoveHorizontal(int step)
+    private void MoveHorizontal(int step) // 수평 방향
     {
         Vector3[,] newPositions = new Vector3[3, 3];
         for (int i = 0; i < 3; i++)
         {
             for (int j = 0; j < 3; j++)
             {
-                int newIndex = (j + step + 3) % 3;
-                newPositions[i, newIndex] = positions[i, j];
+                int newIndex = (j + step + 3) % 3; // 순환적 이동을 위한 새 인덱스 계산
+                newPositions[i, newIndex] = positions[i, j] + new Vector3(mapSize * step, 0, 0); // 실제 위치 변경
             }
         }
         positions = newPositions;
     }
 
-    private void MoveVertical(int step)
+    private void MoveVertical(int step) // 수직 방향
     {
         Vector3[,] newPositions = new Vector3[3, 3];
         for (int i = 0; i < 3; i++)
         {
             for (int j = 0; j < 3; j++)
             {
-                int newIndex = (i + step + 3) % 3;
-                newPositions[newIndex, j] = positions[i, j];
+                int newIndex = (i + step + 3) % 3; // 순환적 이동을 위한 새 인덱스 계산
+                newPositions[newIndex, j] = positions[i, j] - new Vector3(0, mapSize * step, 0); // 실제 위치 변경
             }
         }
         positions = newPositions;
@@ -97,19 +99,19 @@ public class MapRearrangeWithTransform : MonoBehaviour
     {
         if (Input.GetKeyDown(KeyCode.UpArrow))
         {
-            Move(Direction.Up);
+            Move(Direction.UP);
         }
         else if (Input.GetKeyDown(KeyCode.DownArrow))
         {
-            Move(Direction.Down);
+            Move(Direction.DOWN);
         }
         else if (Input.GetKeyDown(KeyCode.LeftArrow))
         {
-            Move(Direction.Left);
+            Move(Direction.LEFT);
         }
         else if (Input.GetKeyDown(KeyCode.RightArrow))
         {
-            Move(Direction.Right);
+            Move(Direction.RIGHT);
         }
     }
 }
