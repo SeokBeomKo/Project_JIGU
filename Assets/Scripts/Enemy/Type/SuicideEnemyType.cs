8c00e15f0e07d39663a9dce543dc6cafea4dbf26
mangruf0927 <pinky0927@naver.com>
2024-04-19 22:37:00 +0900

    Feat : 자폭 몬스터 틀 구현

---
+++ Assets/Scripts/Enemy/Type/SuicideEnemyType.cs
@@ -0,0 +1,108 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SuicideEnemyType : EnemyType
+{
+    [Header("이동 속도")]
+    public float chaseSpeed;
+
+    [Header("사정 거리")]
+    public float shootingRange;
+
+    [Header("공격 준비 시간")]
+    public float attackDelayTime;
+
+    // 추격
+    public override void ChaseEnter()
+    {
+        controller.animator.Play("Chase");
+    }
+    public override void ChaseUpdate()
+    {
+        FlipSprite();
+    }
+    public override void ChaseFixedUpdate()
+    {
+        ChaseTarget(chaseSpeed);
+
+        if (CalculateDistance() <= shootingRange)
+            controller.movementFSM.ChangeState(EnemyStateEnums.ATTACK);
+    }
+    public override void ChaseExit()
+    {
+
+    }
+
+    // 공격 준비
+    public override void AttackPreparationEnter()
+    {
+        controller.animator.Play("AttackPreparation");
+        StartCoroutine(AttackDelay(attackDelayTime, EnemyStateEnums.ATTACK));
+    }
+    public override void AttackPreparationUpdate()
+    {
+        FlipSprite();
+    }
+    public override void AttackPreparationFixedUpdate()
+    {
+    }
+    public override void AttackPreparationExit()
+    {
+
+    }
+
+    // 공격
+    public override void AttackEnter()
+    {
+        controller.animator.Play("Attack");
+    }
+    public override void AttackUpdate()
+    {
+        FlipSprite();
+    }
+    public override void AttackFixedUpdate()
+    {
+        controller.movementFSM.ChangeState(EnemyStateEnums.DEAD);
+    }
+    public override void AttackExit()
+    {
+
+    }
+
+    // 경직
+    public override void StiffenEnter()
+    {
+
+    }
+    public override void StiffenUpdate()
+    {
+
+    }
+    public override void StiffenFixedUpdate()
+    {
+
+    }
+    public override void StiffenExit()
+    {
+
+    }
+
+    // 죽음
+    public override void DeadEnter()
+    {
+
+    }
+    public override void DeadUpdate()
+    {
+
+    }
+    public override void DeadFixedUpdate()
+    {
+
+    }
+    public override void DeadExit()
+    {
+
+    }
+}
